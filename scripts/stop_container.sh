7a93aedd06a78da5fc9659497760f801987de8fd
sangavipriya20 <142722400+sangavipriya20@users.noreply.github.com>
2024-03-15 11:35:22 +0800

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,8 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+# getting the running container id
+containerid='docker ps | awk -F " " '{print $1}''
+# forcefully stopping the container
+docker rm -f $containerid
+
